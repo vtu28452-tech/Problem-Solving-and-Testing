cad95bdf118b2760973468179a6fb0814955a438
vtu28452-tech <vtu28452@veltech.edu.in>
2026-08-22 09:18:53 +0530

    Refactor comparison logic in Comparator

--- Sorting:Comparator.java
+++ Sorting:Comparator.java
@@ -16,12 +16,9 @@ class Checker implements Comparator<Player> {
     @Override
     public int compare(Player a, Player b) {
 
-        // Higher score first
         if (a.score != b.score) {
             return b.score - a.score;
         }
-
-        // Same score -> alphabetical order
         return a.name.compareTo(b.name);
     }
 }
