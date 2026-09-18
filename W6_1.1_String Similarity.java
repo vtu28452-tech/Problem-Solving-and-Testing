0336031c25b0bef2f738a1ad60555361b71e3275
vtu28452-tech <vtu28452@veltech.edu.in>
2026-09-18 09:16:00 +0530

    Create W6_1.1_String Similarity.java

---
+++ W6_1.1_String Similarity.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+
+    public static int stringSimilarity(String s) {
+
+        int n = s.length();
+
+        int[] z = new int[n];
+
+        int left = 0;
+        int right = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= right) {
+                z[i] = Math.min(right - i + 1, z[i - left]);
+            }
+
+            while (i + z[i] < n &&
+                   s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > right) {
+                left = i;
+                right = i + z[i] - 1;
+            }
+        }
+
+        int sum = n;
+
+        for (int i = 1; i < n; i++) {
+            sum += z[i];
+        }
+
+        return sum;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bufferedWriter =
+            new BufferedWriter(
+                new FileWriter(System.getenv("OUTPUT_PATH"))
+            );
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+
+            String s = bufferedReader.readLine();
+
+            int result = Result.stringSimilarity(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
